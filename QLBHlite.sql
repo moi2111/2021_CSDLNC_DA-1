3ed5b8818ba94b158a7a0797f77f2a59daa9bf45
skycanfly06 <pmtri.tvv@outlook.com>
2021-10-23 01:50:47 +0700

    Sua cac trigger

--- QLBHlite.sql
+++ QLBHlite.sql
@@ -8,7 +8,7 @@ GO
 USE QLBH
 GO
 
---TẠO BẢNG VÀ RÀNG BUỘC KHÓA CHÍNH
+-- TẠO BẢNG VÀ RÀNG BUỘC KHÓA CHÍNH
 CREATE TABLE KhachHang
 (
 	MaKH int not null IDENTITY,
@@ -20,40 +20,37 @@ CREATE TABLE KhachHang
 	Phuong nvarchar(50),
 	Quan nvarchar(50),
 	Tpho nvarchar(50),
-	DienThoai char(15)
-	CONSTRAINT PK_KhachHang
-	PRIMARY KEY(MaKH)
+	DienThoai char(15) UNIQUE,
+	CONSTRAINT PK_KhachHang PRIMARY KEY(MaKH)
 )
 CREATE TABLE HoaDon
 (
 	MaHD int not null IDENTITY(1,1),
 	MaKH int,
 	NgayLap datetime,
-	TongTien money not null default 0
-	CONSTRAINT PK_HoaDon
-	PRIMARY KEY(MaHD)
+	TongTien money not null default 0 CHECK (TongTien >= 0)
+	CONSTRAINT PK_HoaDon  PRIMARY KEY(MaHD)
 )
 CREATE TABLE CT_HoaDon
 (
 	MaHD int not null,
 	MaSP int not null,
-	SoLuong int not null default 0,
-	GiaBan money not null default 0,
-	GiaGiam money not null default 0,
-	ThanhTien money not null default 0
-	CONSTRAINT PK_CT_HoaDon
-	PRIMARY KEY(MaHD, MaSP)
+	SoLuong int not null default 0 CHECK (SoLuong >= 0),
+	GiaBan money not null default 0 CHECK (GiaBan >= 0),
+	GiaGiam money not null default 0 CHECK (GiaGiam >= 0),
+	ThanhTien money not null default 0 CHECK (ThanhTien >= 0),
+	CONSTRAINT PK_CT_HoaDon PRIMARY KEY(MaHD, MaSP)
 )
 CREATE TABLE SanPham
 (
 	MaSP int not null IDENTITY,
 	TenSP nvarchar(100),
-	SoLuongTon smallint,
+	SoLuongTon smallint CHECK (SoLuongTon >= 0),
 	Mota nvarchar(max),
-	Gia money not null default 0
-	CONSTRAINT PK_SanPham
-	PRIMARY KEY(MaSP)
+	Gia money not null default 0 CHECK (Gia >= 0)
+	CONSTRAINT PK_SanPham PRIMARY KEY(MaSP)
 )
+GO
 
 --TẠO RÀNG BUỘC KHÓA NGOẠI
 ALTER TABLE HoaDon
@@ -66,104 +63,144 @@ ALTER TABLE CT_HoaDon
 ADD
 	CONSTRAINT FK_CT_HoaDon_HoaDon FOREIGN KEY(MaHD) REFERENCES HoaDon(MaHD),
 	CONSTRAINT FK_CT_HoaDon_SanPham FOREIGN KEY(MaSP) REFERENCES SanPham(MaSP)
-
-ALTER TABLE KhachHang
-ADD
-	CONSTRAINT U_DienThoai UNIQUE(DienThoai)
-
---STORED PROCEDURE
 GO
-CREATE PROCEDURE sp_updateCT_HoaDon
-AS--UPDATE(GiaBan,GiaGiam)
-	IF(EXISTS(SELECT * FROM CT_HoaDon CT, SanPham SP WHERE CT.MaSP = SP.MaSP))
-	BEGIN
-		DECLARE @MaSP int, @GiaBan money, @GiaGiam money, @Min int, @Max int
-		SELECT @MaSP = CT.MaSP, @GiaBan = SP.Gia
-				FROM CT_HoaDon CT, SanPham SP WHERE CT.MaSP = SP.MaSP
-		SET @Min = 1
-		SET @Max = 10
-		SET @GiaGiam = (@GiaBan * FLOOR(RAND()*(@Max-@Min+1)+@Min)) / 100
-		UPDATE CT_HoaDon
-		SET GiaBan = @GiaBan, GiaGiam = @GiaGiam
-		WHERE MaSP = @MaSP
-	END
-GO 
 
---TRIGGER
-/*a. Thành tiền CTHD = (Số lượng * (Giá bán - Giá giảm))
+/*
+-- TRIGGER --
+a. Thành tiền CTHD = (Số lượng * (Giá bán - Giá giảm))
 Bảng TAH:
-			|	T	|	X	|		S
+			|	T	|	X	|	S
 ____________|_______|_______|___________________________________
-CT_HoaDon	|	+	|	-	|+(SoLuong,GiaBan,GiaGiam,ThanhTien)
+CT_HoaDon	|	+	|	-	|	+ (SoLuong,GiaBan,GiaGiam,ThanhTien)
+
+b. Tổng tiền trong hóa đơn = tổng thanh toán từng chi tiết hóa đơn
+Bảng TAH:
+			|	T	|	X	|	S
+____________|_______|_______|________________________________________
+HoaDon		|	+	|	-	|	+ (TongTien)
+____________|_______|_______|________________________________________
+CT_HoaDon	|	+	|	+	|	+ (MaSP,SoLuong,GiaBan,GiaGiam,ThanhTien)
+
+--------------------------------------------
+
+CÀI TRIGGER CHO BẢNG HOADON
+(Insert)
+Ràng buộc: 
+	-	tongtien của một hoadon vừa insert phải bằng 0 (vì chưa có ct_hoadon)
+Thực hiện:
+	-	Kiểm tra ràng buộc. Nếu sai, rollback
 */
-CREATE TRIGGER tg_CapNhatThanhTien ON CT_HoaDon FOR INSERT, UPDATE
+GO
+CREATE TRIGGER tg_HoaDon_Insert ON HoaDon 
+	AFTER INSERT
 AS
-	IF (UPDATE(SoLuong) OR UPDATE(GiaBan) OR UPDATE(GiaGiam) OR UPDATE(ThanhTien))
+	IF EXISTS (SELECT * FROM INSERTED i WHERE i.TongTien != 0)
 	BEGIN
-		UPDATE CT_HoaDon
-			SET ThanhTien = SoLuong * (GiaBan - GiaGiam)
-			WHERE EXISTS(SELECT * FROM INSERTED I
-						 WHERE I.MaHD = CT_HoaDon.MaHD AND I.MaSP = CT_HoaDon.MaSP)
+		RAISERROR('Ban da insert mot hoadon co tongtien khac 0', 0, 1)
+		ROLLBACK
 	END
 GO
 
 /*
-b. Tổng tiền trong hóa đơn = tổng thanh toán từng chi tiết hóa đơn
-Bảng TAH:
-			|	T	|	X	|		S
-____________|_______|_______|________________________________________
-HoaDon		|	-	|	-	|+(TongTien)
-____________|_______|_______|________________________________________
-CT_HoaDon	|	+	|	+	|+(MaSP,SoLuong,GiaBan,GiaGiam,ThanhTien)
+(Update)
+Ràng buộc: 
+	-	tongtien của hoadon được update phải bằng tổng các thành tiền của các ct_hoadon tương ứng
+Thực hiện
+	-	Kiểm tra ràng buộc. Nếu sai, rollback
 */
-
-GO
-CREATE TRIGGER tg_HoaDon_update ON HoaDon AFTER UPDATE
+CREATE TRIGGER tg_HoaDon_Update ON HoaDon
+	AFTER UPDATE
 AS
-	DECLARE @TongTien money
-	DECLARE @MaHD int
-	SELECT @MaHD = MaHD, @TongTien = TongTien FROM INSERTED
-
-	IF @TongTien != (SELECT SUM(ThanhTien) FROM CT_HoaDon WHERE MaHD = @MaHD)
+	IF (UPDATE(TongTien))
 	BEGIN
-		RAISERROR('TongTien khong bang tong ThanhToan', 15, 1)
-		ROLLBACK
+		IF EXISTS (SELECT * 
+				   FROM INSERTED i LEFT JOIN CT_HoaDon ct ON i.MaHD = ct.MaHD
+				   GROUP BY i.MaHD, i.TongTien
+				   HAVING i.TongTien != COALESCE(SUM(ct.thanhtien), 0))
+		BEGIN
+			RAISERROR('Update hoadon co tongtien khong bang tong thantien cua cac ct_hoadon tuong ung', 15, 1)
+			ROLLBACK
+		END
 	END
----
 GO
-CREATE TRIGGER tg_CTHoaDon_insert ON CT_HoaDon AFTER INSERT
-AS
-	EXEC sp_updateCT_HoaDon --UPDATE CT_HoaDon(GiaBan, GiaGiam)
-	UPDATE HoaDon
-		SET TongTien = TongTien + (SELECT ThanhTien FROM INSERTED WHERE MaHD = HoaDon.MaHD)
-		FROM HoaDon JOIN INSERTED ON HoaDon.MaHD = INSERTED.MaHD
----
-GO
-CREATE TRIGGER tg_CTHoaDon_delete ON CT_HoaDon FOR DELETE
-AS
-	UPDATE HoaDon
-	SET TongTien = TongTien - 
-		(SELECT ThanhTien FROM DELETED WHERE MaHD = HoaDon.MaHD)
-	FROM HoaDon JOIN DELETED ON HoaDon.MaHD = DELETED.MaHD
----
-GO
-CREATE TRIGGER tg_CTHoaDon_update ON CT_HoaDon AFTER UPDATE
-AS
-	IF UPDATE(MaSP)
-		EXEC sp_updateCT_HoaDon --UPDATE SanPham tương ứng
-	UPDATE HoaDon
-	SET TongTien = TongTien + 
-		(SELECT ThanhTien FROM INSERTED WHERE MaHD = HoaDon.MaHD) -
-		(SELECT ThanhTien FROM DELETED WHERE MaHD = HoaDon.MaHD)
-	FROM HoaDon JOIN DELETED ON HoaDon.MaHD = DELETED.MaHD
----
-GO
-sp_settriggerorder @triggername = 'tg_CapNhatThanhTien', @order = 'last', @stmttype = 'INSERT'
-GO
-sp_settriggerorder @triggername = 'tg_CapNhatThanhTien', @order = 'last', @stmttype = 'UPDATE'
 
---NHẬP DỮ LIỆU
+/*
+CÀI TRIGGER CHO CT_HOADON
+(Insert | Update)
+Ràng buộc:
+	-	giaban = gia(sanpham)						
+	-	thanhtien = soluong * (giaban - giagiam)	 
+	-	tongtien(hoadon) = sum(thanhtien) 
+Thực hiện:
+	-	Nếu là insert: cập nhật thông tin giaban, giagiam, thanhtien, tongtien(hoadon)
+	-	Nếu là update: cập nhật lại thông tin giaban, thanhtien, tongtien(hoadon)	
+*/
+CREATE TRIGGER tg_CtHoaDon_Insert_Update ON CT_HoaDon
+	AFTER INSERT, UPDATE
+AS
+	-- Nếu là hành động insert (chỉ có insert thì bảng deleded mới rỗng)
+	IF NOT EXISTS (SELECT * FROM DELETED) 
+	BEGIN  
+		-- Update lại giaban từ gia(sanpham)
+    	UPDATE CT_HoaDon
+			SET CT_HoaDon.GiaBan = (SELECT sp.Gia FROM SanPham sp WHERE sp.MaSP = CT_HoaDon.MaSP)
+			WHERE EXISTS (SELECT * FROM INSERTED i WHERE i.MaHD = CT_HoaDon.MaHD AND i.MaSP = CT_HoaDon.MaSP)
+		-- Tạo giagiam (bằng 0%, 5%, 10% giaban), update thanhtien = soluong * (giaban - giagiam)
+		UPDATE CT_HoaDon
+			SET CT_HoaDon.GiaGiam = CT_HoaDon.GiaBan * (FLOOR(RAND() * 3) * 5) / 100,
+			ThanhTien = SoLuong * (GiaBan - GiaGiam)
+			WHERE EXISTS (SELECT * FROM INSERTED i WHERE i.MaHD = CT_HoaDon.MaHD AND i.MaSP = CT_HoaDon.MaSP)
+		-- Update tongtien(hoadon)
+		UPDATE HoaDon 
+			SET HoaDon.TongTien = (SELECT COALESCE(SUM(ct.ThanhTien), 0)
+							   	       FROM CT_HoaDon ct
+									   WHERE ct.MaHD = HoaDon.MaHD)
+			WHERE HoaDon.mahd IN (SELECT i.mahd FROM INSERTED i)
+    END
+	-- Nếu là hành động update
+    ELSE
+	BEGIN
+		-- Nếu có thay đổi các thông tin liên quan đến thanhtien thì tính lại số liệu 
+		IF UPDATE(giaban) OR UPDATE(masp) OR UPDATE(soluong) OR UPDATE(giagiam) OR UPDATE(thanhtien)
+		BEGIN
+			-- Nếu có thay đổi tại giaban hoặc masp -> update lại giaban phù hợp
+			IF UPDATE(GiaBan) OR UPDATE(MaSP)
+			BEGIN
+				UPDATE CT_HoaDon
+					SET CT_HoaDon.GiaBan = (SELECT sp.Gia FROM SanPham sp WHERE sp.MaSP = CT_HoaDon.MaSP)
+					WHERE EXISTS (SELECT * FROM INSERTED i WHERE i.MaHD = CT_HoaDon.MaHD AND i.MaSP = CT_HoaDon.MaSP)
+			END
+			-- Update thanhtien
+			UPDATE CT_HoaDon
+				SET ThanhTien = SoLuong * (GiaBan - GiaGiam)
+				WHERE EXISTS (SELECT * FROM INSERTED i WHERE i.MaHD = CT_HoaDon.MaHD AND i.MaSP = CT_HoaDon.MaSP)
+        END		
+		-- Update lại tongtien(hoadon)
+		UPDATE HoaDon 
+			SET HoaDon.TongTien = (SELECT COALESCE(SUM(ct.ThanhTien), 0) 
+								       FROM CT_HoaDon ct 
+									   WHERE ct.MaHD = HoaDon.MaHD)
+			WHERE HoaDon.mahd IN (SELECT D.mahd FROM DELETED d) OR HoaDon.mahd IN (SELECT i.MaHD FROM INSERTED i)
+	END
 GO
+
+/*
+(Delete)
+Ràng buộc: 
+	-	khi xoá một ct_hoadon, tongtien của hoadon tương ứng trừ đi thanhtien
+Thực hiện:
+	-	Cập nhật tongtien của hoadon tương ứng với ct_hoadon được xoá
+*/
+CREATE TRIGGER tg_CTHoaDon_Delete ON CT_HoaDon 
+	AFTER DELETE
+AS
+	UPDATE HoaDon
+		SET HoaDon.TongTien -= (SELECT SUM(D.ThanhTien) FROM DELETED d WHERE D.MaHD = HoaDon.MaHD)
+		WHERE HoaDon.mahd IN (SELECT D.MaHD FROM DELETED d)
+GO
+
+
+-- NHẬP DỮ LIỆU
 SET DATEFORMAT YMD
 SET IDENTITY_INSERT dbo.KhachHang ON
 INSERT dbo.KhachHang(MaKH, Ho, Ten, Ngsinh, SoNha, Duong, Phuong, Quan, Tpho, DienThoai) VALUES (1, N'Trương', N'Huy Đức', '1974-05-26 00:00:00.000', '843F', N'Hòa Mỹ', N'Phường 11', N'Quận 5', N'Kon Tum', '0569277696')
@@ -271,8 +308,8 @@ INSERT dbo.HoaDon(MaHD, MaKH, NgayLap) VALUES (50, 5, '2021-06-08 14:59:59.000')
 
 SET IDENTITY_INSERT HoaDon OFF
 --SELECT * FROM HoaDon
+--SELECT * FROM CT_HoaDon chd
 
-INSERT dbo.CT_HoaDon(MaHD, MaSP, SoLuong) VALUES (1, 2, 1)
 INSERT dbo.CT_HoaDon(MaHD, MaSP, SoLuong) VALUES (1, 1, 2)
 INSERT dbo.CT_HoaDon(MaHD, MaSP, SoLuong) VALUES (2, 3, 3)
 INSERT dbo.CT_HoaDon(MaHD, MaSP, SoLuong) VALUES (1, 4, 2)
